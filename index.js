fcadc1c909d003b8cc375b8d7025d223b7e0a0d9
Aom Kham Phaung <akhamphaung@gmail.com>
2024-02-28 14:07:59 +0000

    submit: connect db and create user

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv').config()
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGO_URI)
+      .then(() => console.log("Connected to database"))
+      .catch((err) => console.log(err))
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  }
+})
+
+const exerciseSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+const User = mongoose.model('User', userSchema);
+const Exercise = mongoose.model('Exercise', exerciseSchema);
 
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+
+app.post('/api/users', async(req, res) => {
+  try{
+    const newUser = await User.create(req.body);
+    res.status(201).json({data: newUser});
+  }catch(err) {
+    console.log(err);
+    res.status(500).json("Cannot create user");
+  }
+})
 
 
 
